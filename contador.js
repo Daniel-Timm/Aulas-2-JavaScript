9cb454cedc5078f2e2f95a3fb55703cb152e587d
Daniel Timm <danielg1timm@gmail.com>
2025-10-02 14:20:07 -0300

    Add files via upload

---
+++ contador.js
@@ -0,0 +1,31 @@
+const PromptSync = require('prompt-sync');
+
+var prompt = require('prompt-sync')();
+
+
+// let palavra = "";
+// let i = 0;
+// while (palavra == "oi")
+//     {senha = +prompt ("Digite sua palavra:  ")
+//      i = i + 1 ;
+//      console.log(i);
+     
+//     }
+    
+//     console.log("Palavra errada");
+
+
+// const repeticoes = +prompt ("Digite quantas vezes: ")
+// let i = 0;
+// while    (i <= repeticoes){
+//     console.log("oi");
+//     i++
+// }
+
+
+
+
+for (let i = 1; i <= 20 ; i++) {
+    
+    console.log(i)
+}
